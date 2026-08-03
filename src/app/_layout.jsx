5a95b9e26d5e0ceca1a4b06a3e39d2167365e3c1
zain-dev25 <minsarshah1@gmail.com>
2026-08-03 17:55:10 +0500

    create screens

---
+++ src/app/_layout.jsx
@@ -0,0 +1,14 @@
+import { Slot, Stack } from "expo-router";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+     <Stack screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="index" />
+    <Stack.Screen name="testing" />
+     </Stack>
+    </SafeAreaProvider>
+  );
+}
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,24 @@
-import { View } from "react-native-web";
+import { useRouter } from "expo-router";
+import { Touchable } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { Text } from "react-native";
+import { View } from "react-native";
 
 export default function HomeScreen() {
+  const router = useRouter();
   return (
-  <View >
-    Home Screen
-  </View>
-);
+    <>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+        
+        <TouchableOpacity
+          style={{ padding: 10, backgroundColor: "lightgray" }}
+          onPress={() => router.push("/testing")}
+        >
+          {" "}
+          <Text>Press me</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
 }
