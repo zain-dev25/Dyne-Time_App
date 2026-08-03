f86d44bd2cae7083acb4931ce5b3bc64c13e9092
zain-dev25 <minsarshah1@gmail.com>
2026-08-03 17:29:22 +0500

    start work

---
+++ src/app/index.jsx
@@ -0,0 +1,9 @@
+import { View } from "react-native-web";
+
+export default function HomeScreen() {
+  return (
+  <View >
+    Home Screen
+  </View>
+);
+}
---
+++ src/components/UserData.jsx
@@ -0,0 +1,63 @@
+// user data with name , id and their short description and email
+export const userData = [
+  {
+    id: '1',
+    name: 'John Doe',
+    description: 'Software Engineer',
+    email: 'john.doe@example.com'
+  },
+  {
+    id: '2',
+    name: 'Jane Smith',
+    description: 'Product Manager',
+    email: 'jane.smith@example.com'
+  },
+  {
+    id: '3',
+    name: 'Alice Johnson',
+    description: 'UX Designer',
+    email: 'alice.johnson@example.com'
+  },
+  {
+    id: '4',
+    name: 'Bob Brown',
+    description: 'Data Scientist',
+    email: 'bob.brown@example.com'
+  },
+  {
+    id: '5',
+    name: 'Charlie Davis',
+    description: 'DevOps Engineer',
+    email: 'charlie.davis@example.com'
+  },
+    {
+    id: '6',
+    name: 'Eve Wilson',
+    description: 'QA Engineer',
+    email: 'eve.wilson@example.com'
+  },
+  {
+    id: '7',
+    name: 'Frank Miller',
+    description: 'Business Analyst',
+    email: 'frank.miller@example.com'
+  },
+  {
+    id: '8',
+    name: 'Grace Lee',
+    description: 'Marketing Specialist',
+    email: 'grace.lee@example.com'
+  },
+  {
+    id: '9',
+    name: 'Hannah Taylor',
+    description: 'Content Writer',
+    email: 'hannah.taylor@example.com'
+  },
+  {
+    id: '10',
+    name: 'Ian Anderson',
+    description: 'Sales Manager',
+    email: 'ian.anderson@example.com'
+  }
+];
