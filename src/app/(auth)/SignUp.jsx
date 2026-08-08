03397d1b52a35b4313c157e91cba1ed7fef2d547
zain-dev25 <minsarshah1@gmail.com>
2026-08-08 23:45:15 +0500

    Creage email and password unput using with formik and yup

--- src/app/(auth)/SignUp.jsx
+++ src/app/(auth)/SignUp.jsx
@@ -1,10 +1,15 @@
 import { Link, useRouter } from "expo-router";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Logo from "../../images/dinetimelogo.png";
 import frameImage from "../../images/Frame.png";
+import { Formik } from "formik";
+import { authSchema } from "../../../utils/authScheema";
 
 export default function SignUp() {
+  const handleSignUp=()=>{
+    // Handle sign up logic here
+  }
   const router = useRouter();
   return (
     <>
@@ -14,22 +19,56 @@ export default function SignUp() {
           <Image source={Logo} style={style.logo} />
         </View>
 
-        {/*  Buttons */}
         <View>
-          <TouchableOpacity
-            onPress={() => router.push("/(auth)/SignUp")}
-          style={style.button}
-          >
-            <Text style={style.text}> Sing up</Text>
-          </TouchableOpacity>
-
-          
-          <TouchableOpacity
-            onPress={() => router.push("#")}
-          >
-            <Text style={style.text2}> Guest User</Text>
-          </TouchableOpacity>
+          <Text style={style.text}>Let's get you set up!</Text>
         </View>
+        {/*  Inputs */}
+
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={authSchema}
+          onSubmit={(handleSignUp) => {
+            // router.push('/(auth)/SignIn');
+          }}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values,errors , touched }) => (
+            <View style={{ marginTop: 20 }}>
+              <Text style={style.text2}>Email</Text>
+              <TextInput
+                placeholder="Enter Your Email" 
+                placeholderTextColor="white"
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                keyboardType="email-address"
+                value={values.email}
+                style={style.input}
+              />
+              <Text style={{ color: 'red', marginTop: 5 }}>{errors.email && touched.email && errors.email}</Text>
+            
+            
+              <Text style={style.text2}>Password</Text>
+              <TextInput
+                placeholder="Enter Your Password"
+                placeholderTextColor="white"
+                secureTextEntry
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+                style={style.input}
+              />
+              <Text style={{ color: 'red', marginTop: 2 }}>{errors.password && touched.password && errors.password}</Text>
+              <TouchableOpacity onPress={handleSubmit}>
+                <Text style={style.signup}>Sign Up</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleSubmit}>
+                <Text style={style.signin}>Sign In</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+
+
+        </Formik>
+
 
         {/* OR */}
         <View style={style.orSection}>
@@ -42,12 +81,12 @@ export default function SignUp() {
         <View>
           <Text style={style.alreadyAccount}>
             {" "}
-            Already have an account?{" "}
+            Be a{" "}
             <Link
               href="/(auth)/SignIn"
-              style={{ color: "#f49b33", fontWeight: "bold" }}
+              style={{ color: "#f49b33",textDecorationLine: "underline", fontWeight: "bold" }}
             >
-              Log in
+              Guest User
             </Link>
           </Text>
         </View>
@@ -66,46 +105,53 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#2b2b2b",
-    marging: 20,
     padding: 20,
   },
   logo: {
-    width: 350,
-    height: 200,
+    width: 250,
+    height: 150,
     alignSelf: "center",
-    marginTop: 50,
+    marginTop: 5,
   },
   text: {
     textAlign: "center",
-    backgroundColor: "#f49b33",
-    padding: 10,
+    padding: 0,
     borderRadius: 5,
-    color: "black",
-    marginTop: 20,
+    color: "white",
+    marginTop: -40,
     alignItems: "center",
     fontWeight: "bold",
-    fontSize: 16,
+    fontSize: 23,
   },
   text2: {
-    textAlign: "center",
+    textAlign: "start",
     borderColor: "#f49b33",
-    borderWidth: 2,
     padding: 10,
     borderRadius: 5,
     color: "#f49b33",
-    marginTop: 20,
     alignItems: "center",
     fontWeight: "bold",
-    fontSize: 16,
+    fontSize: 14,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#f49b33",
+    borderRadius: 5,
+    color: "white",
+    marginTop: 0,
+    backgroundColor: "#2b2b2b",
+    textAlign: "left",
+    paddingLeft: 15,
+    
   },
   button: {
     borderRadius: 5,
-    marginTop: 40,
+    marginTop: 30,
   },
   alreadyAccount: {
     textAlign: "center",
     color: "white",
-    marginTop: 20,
+    marginTop: 10,
     fontSize: 16,
   },
   whiteSpace: {
@@ -122,12 +168,34 @@ const style = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 20,
+    marginTop: 10,
   },
   frameImage: {
     width: 350,
     height: 80,
     alignSelf: "center",
     marginTop: 50,
+    position: "absolute",
+    bottom: -130,
   },
+  signup:{
+    color: 'white',
+    textAlign: 'center',
+    padding: 10,
+    backgroundColor: '#f49b33',
+    borderRadius: 5,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+    
+  },
+  signin:{
+    color: '#f49b33',
+    textAlign: 'center',
+    padding: 10,
+    borderRadius: 5,
+    fontWeight: 'bold',
+    borderColor: '#f49b33',
+    borderWidth: 1,
+  }
 });
